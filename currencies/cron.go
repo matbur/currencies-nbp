2698811946def8185effa68dae2a6e02e9db1966
matbur <mb@facta.io>
2023-04-14 15:50:22 +0200

    Fix cron

--- currencies/cron.go
+++ currencies/cron.go
@@ -4,6 +4,6 @@ import "encore.dev/cron"
 
 var _ = cron.NewJob("fetch-currencies", cron.JobConfig{
 	Title:    "Fetch currencies",
-	Endpoint: Service.SaveCurrent,
+	Endpoint: SaveCurrent,
 	Schedule: "5 12 * * 1-5",
 })
