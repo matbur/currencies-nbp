21b9cecaa4c8ad15d2ca1982a3d6494ad14a34a6
matbur <mb@facta.io>
2023-04-14 10:54:17 +0200

    Remove cron

--- currencies/cron.go
+++
@@ -1,9 +0,0 @@
-package currencies
-
-import "encore.dev/cron"
-
-var _ = cron.NewJob("fetch-currencies", cron.JobConfig{
-	Title:    "Fetch currencies",
-	Endpoint: Service.SaveCurrent,
-	Schedule: "5 12 * * 1-5",
-})
